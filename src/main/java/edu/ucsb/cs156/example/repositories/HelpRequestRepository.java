8978ff713b05e918c120b197b27cfea8feaaa3ac
SloggyYoggy <adrumm825@gmail.com>
2024-05-01 16:20:50 -0700

    first commit for branch

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -1,12 +1,13 @@
-package edu.ucsb.cs156.example.repositories;
+// package edu.ucsb.cs156.example.repositories;
+package main.java.edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.HelpRequest;
+import edu.ucsb.cs156.example.entities.HelpRequested;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {
-    
+public interface HelpRequestRepository extends CrudRepository<HelpRequested, Long> {
+
 }
