1983f1390e14a334a674cf7efc8a1caf865c1481
kylume <123840358+kylume@users.noreply.github.com>
2024-05-01 17:43:37 -0700

    add controller and controller tests for recommendationRequest

--- src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -1,8 +1,8 @@
 package edu.ucsb.cs156.example.controllers;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.entities.RecommendationRequest;
 import edu.ucsb.cs156.example.errors.EntityNotFoundException;
-import edu.ucsb.cs156.example.repositories.UCSBDateRepository;
+import edu.ucsb.cs156.example.repositories.RecommendationRequestRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -22,3 +22,56 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import java.time.LocalDateTime;
+
+@Tag(name = "RecommendationRequest")
+@RequestMapping("/api/RecommendationRequest")
+@RestController
+@Slf4j
+public class RecommendationRequestController extends ApiController {
+
+    @Autowired
+    RecommendationRequestRepository recommendationRequestRepository;
+
+    @Operation(summary= "List all Recommendation Letter requests")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<RecommendationRequest> allRecommendationRequests() {
+        Iterable<RecommendationRequest> recs = recommendationRequestRepository.findAll();
+        return recs;
+    }
+
+    @Operation(summary= "Create a new Recommendation Letter request")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public RecommendationRequest postRecommendationRequest(
+            @Parameter(name="requesterEmail") @RequestParam String requesterEmail,
+            @Parameter(name="professorEmail") @RequestParam String professorEmail,
+            @Parameter(name="explanation") @RequestParam String explanation,
+            @Parameter(name="dateRequested (iso format)") @RequestParam("dateRequested") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateRequested,
+            @Parameter(name="dateNeeded (iso format)") @RequestParam("dateNeeded") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateNeeded,
+            @Parameter(name="done") @RequestParam boolean done)
+            throws JsonProcessingException {
+
+        // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+        // See: https://www.baeldung.com/spring-date-parameters
+
+        log.info("dateRequested={}", dateRequested);
+        log.info("dateNeeded={}", dateNeeded);
+
+        RecommendationRequest rec = new RecommendationRequest();
+        rec.setRequesterEmail(requesterEmail);
+        rec.setProfessorEmail(professorEmail);
+        rec.setExplanation(explanation);
+        rec.setDateRequested(dateRequested);
+        rec.setDateNeeded(dateNeeded);
+        rec.setDone(done);
+
+        RecommendationRequest savedRec = recommendationRequestRepository.save(rec);
+
+        return savedRec;
+    }
+}
